82b5ad263a028667743d1eb02f8981ef3815a935
Stanislav Spuzyak <stanislav.spuzyak@gmail.com>
2016-09-30 15:29:24 -0400

    update: add retriev user password function to auth service

--- server/core/auth.js
+++ server/core/auth.js
@@ -2,6 +2,10 @@ const userBase = require('../data/user');
 const passManager = require('./password');
 const jwt = require('jsonwebtoken');
 
+function getUserPasswordHash(user) {
+    return userBase.credentials[user];
+}
+
 module.exports = function (req, res) {
     if (userBase.authUsers.indexOf(req.body.user) < 0) {
         return res
@@ -12,7 +16,7 @@ module.exports = function (req, res) {
             });
     }
 
-    if (!passManager.validate(userBase.credentials[req.body.user], req.body.password)) {
+    if (!passManager.validate(getUserPasswordHash(req.body.user), req.body.password)) {
         return res
             .status('401')
             .send({
