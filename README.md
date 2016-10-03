6b4335f66becdee8ba4d88e4ef452332b542acce
Stanislav Spuzyak <stanislav.spuzyak@gmail.com>
2016-10-02 20:38:36 -0400

    feature: introduce mongodb to project

--- README.md
+++ README.md
@@ -28,3 +28,14 @@ Test project code base:
 
     # when develop in TDD style
     npm run tdd
+
+## Prepare database
+
+Mongodb holds application model. You should create **"local"** database and **"jwt"** collection before start.
+Mondodb is running on default 27017 port.
+
+Initial data set:
+
+    collection.insert([
+        { user: 'dartwader', password: '8We4bOMkPf9be089cdbe9dd86968ef070df7d3402' }
+    ]);
--- server/core/auth.js
+++ server/core/auth.js
@@ -1,34 +1,73 @@
 const userBase = require('../data/user');
 const passManager = require('./password');
 const jwt = require('jsonwebtoken');
+const MongoClient = require('mongodb').MongoClient;
 
-function getUserPasswordHash(user) {
-    return userBase.credentials[user];
+function _errorHandler(response) {
+    return (status, data) => {
+        return response
+            .status(status)
+            .send(data);
+    };
 }
 
-module.exports = function (req, res) {
-    if (userBase.authUsers.indexOf(req.body.user) < 0) {
-        return res
-            .status('404')
-            .send({
+function _checkUser(entity, input, callback) {
+    if (entity === null) {
+        callback(
+            '404',
+            {
                 status: 'error',
                 message: 'unknown user'
-            });
+            }
+        );
+
+        return false;
     }
 
-    if (!passManager.validate(getUserPasswordHash(req.body.user), req.body.password)) {
-        return res
-            .status('401')
-            .send({
+    if (!passManager.validate(entity.password, input.password)) {
+        callback(
+            '401',
+            {
                 status: 'error',
                 message: 'wrong password'
-            });
+            }
+        );
+
+        return false;
     }
 
-    return res
-        .status('200')
-        .send({
-            status: 'success',
-            token: jwt.sign({ user: req.body.user }, 'secret')
+    return true;
+}
+
+module.exports = function (request, response) {
+    // connect to the "local" database
+    MongoClient.connect('mongodb://localhost:27017/local', (error, db) => {
+        if (error) {
+            throw error;
+        }
+
+        db.collection('jwt').findOne({ user: request.body.user }, (error, user) => {
+            if (error) {
+                return response
+                    .status('500')
+                    .send({
+                        status: 'error',
+                        message: 'something happend with the server :('
+                    });
+            }
+
+            if (_checkUser(user, request.body, _errorHandler(response))) {
+                db.close();
+
+                return response
+                    .status('200')
+                    .send({
+                        status: 'success',
+                        token: jwt.sign({ user: request.body.user }, 'secret')
+                    });
+            }
+
+            db.close();
         });
+    });
 };
